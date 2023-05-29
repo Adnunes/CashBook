8f0588b45d074a395074cfe489ce6707a6545aa4
Andre Nunes <andred_nunes@hotmail.com>
2023-05-29 14:41:27 -0300

    other changes to test command line skills

--- cash_flow.py
+++ cash_flow.py
@@ -29,29 +29,10 @@ class AccountingEntry:
         AccountingEntry.cod +=1
         self.cash_book = cash_book
 
-    def set_entry(self):
+    def set_entry(self) -> None:
         if self.is_debit:
             self.cash_book.balance += self.value
             self.cash_book.debits[self.cod] = [self.date, self.description, self.value]
         else:
             self.cash_book.balance -= self.value
             self.cash_book.credits[self.cod] = [self.date, self.description, self.value]
-
-
-cash_book_1 = CashBook('Empresa Abc')
-
-lancamento_1 = AccountingEntry('29/05/2023', 'primeiro lancamento',True,124, cash_book_1)
-lancamento_1.set_entry()
-
-lancamento_2 = AccountingEntry('29/05/2023', 'segundo lancamento',True,134, cash_book_1)
-lancamento_2.set_entry()
-
-lancamento_3 = AccountingEntry('29/05/2023', 'terceiro lancamento',False,8, cash_book_1)
-lancamento_3.set_entry()
-
-lancamento_4 = AccountingEntry('29/05/2023', 'quarto lancamento',False,10, cash_book_1)
-lancamento_4.set_entry()
-
-
-
-print(cash_book_1)
